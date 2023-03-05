02a7217f54252998b3d16bd2841e46f905f180ef
Dimitri Bartone <dimitribartone@hotmail.fr>
2023-03-05 18:37:21 +0100

    Scripts updated (pages/index.js + factory/photographers.js) to import the photographers datas (name, portrait, city, country, tagline, price) + photographers portraits added + CSS/HTML updated

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,27 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const userPortrait = document.createElement("img");
+    userPortrait.setAttribute("src", picture);
+    const userName = document.createElement("h2");
+    const userLocation = document.createElement("p");
+    const userTagline = document.createElement("p");
+    const userPrice = document.createElement("p");
+    userName.textContent = name;
+    userLocation.textContent = city + ", " + country;
+    userTagline.textContent = tagline;
+    userPrice.textContent = price + "€/jour";
+    article.appendChild(userPortrait);
+    article.appendChild(userName);
+    article.appendChild(userLocation);
+    article.appendChild(userTagline);
+    article.appendChild(userPrice);
+
+    return (article);
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
